c2ed6e5ab68cc9eae9e0e84752f315ee8aed5127
Chuan Igor <38382865+Syggnus@users.noreply.github.com>
2023-04-12 19:04:01 -0300

    feat: Consumindo a API do randomuser

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import Header from "@/components/header";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const urlBase = "https://randomuser.me/api/?results=";
+  const [users, setUsers] = useState([]);
+  const [userAmount, setUserAmount] = useState(2);
+  
+  useEffect(() => {
+    async function fetchDataAmount() {
+      const response = await fetch(`${urlBase}${userAmount}`);
+      const responseAmount = await response.json();
+      setUsers(responseAmount.results);
+    }
+    fetchDataAmount();
+  }, [userAmount]);
+
+  console.log(users)
+
   return (
     <>
       <Header />
