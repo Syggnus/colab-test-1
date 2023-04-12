4a0c5896840ebc92b7a7017b468f09726512ef1f
Chuan Igor <38382865+Syggnus@users.noreply.github.com>
2023-04-12 19:31:53 -0300

    feat: Utilização de Cards para Exibir os Dados dos Usuários

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,75 @@
 import Header from "@/components/header";
+import { MultipleRandomUser, User } from "@/interface/IRandomuser";
+import { Card, Tooltip } from "flowbite-react";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const urlBase = "https://randomuser.me/api/?results=";
-  const [users, setUsers] = useState([]);
-  const [userAmount, setUserAmount] = useState(2);
-  
+  const [users, setUsers] = useState<User[]>([]);
+  const [userAmount, setUserAmount] = useState(10);
+
   useEffect(() => {
     async function fetchDataAmount() {
       const response = await fetch(`${urlBase}${userAmount}`);
-      const responseAmount = await response.json();
+      console.log(response);
+      const responseAmount: MultipleRandomUser = await response.json();
       setUsers(responseAmount.results);
     }
     fetchDataAmount();
   }, [userAmount]);
 
-  console.log(users)
-
   return (
     <>
       <Header />
+      <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="grid grid-cols-4 max-md:grid-cols-2 max-sm:grid-cols-1 grid-rows-4 gap-4">
+          {users.map((user: User) => {
+            return (
+              <>
+                <div className="max-w-sm">
+                  <Card>
+                    <div className="flex flex-col items-center pb-10 px-4 pt-4">
+                      <Image
+                        className="mb-3 h-24 w-24 rounded-full shadow-lg"
+                        src={user.picture.large}
+                        alt="Bonnie image"
+                        width={150}
+                        height={150}
+                      />
+                      <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                        {user.name.first} {user.name.last}
+                      </h5>
+                      <span className="text-sm text-gray-500 dark:text-gray-400">
+                        {user.gender == "female" ? "Feminino" : "Masculino"} -{" "}
+                        {user.dob.age} anos
+                      </span>
+                      <div className="mt-4 flex space-x-3 lg:mt-6">
+                        <Tooltip
+                          trigger="hover"
+                          content={`Endereço: ${user.location.street.name}  ${user.location.street.number}, ${user.location.city} - ${user.location.state} - ${user.location.country}`}
+                        >
+                          <div className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Localização
+                          </div>
+                        </Tooltip>
+                        <Tooltip
+                          trigger="hover"
+                          content={`Telefone: ${user.phone} - Celular: ${user.cell}`}
+                        >
+                          <div className="inline-flex items-center rounded-lg border border-gray-300 bg-white py-2 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+                            Contato
+                          </div>
+                        </Tooltip>
+                      </div>
+                    </div>
+                  </Card>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
