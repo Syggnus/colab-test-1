b5efde57a154f1eac2cca890c5ee506db769713c
Chuan Igor <38382865+Syggnus@users.noreply.github.com>
2023-04-12 19:43:39 -0300

    refactor: Criação de Componente ListCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
+import ListCard from "@/components/ListCard";
 import Header from "@/components/header";
 import { MultipleRandomUser, User } from "@/interface/IRandomuser";
-import { Card, Tooltip } from "flowbite-react";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -22,56 +21,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <div className="grid grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 grid-rows-4 gap-4">
-          {users.map((user: User) => {
-            return (
-              <>
-                <div className="max-w-sm">
-                  <Card>
-                    <div className="flex flex-col items-center pb-10 px-4 pt-4">
-                      <Image
-                        className="mb-3 h-24 w-24 rounded-full shadow-lg"
-                        src={user.picture.large}
-                        alt="Bonnie image"
-                        width={150}
-                        height={150}
-                      />
-                      <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                        {user.name.first} {user.name.last}
-                      </h5>
-                      <span className="text-sm text-gray-500 dark:text-gray-400">
-                        {user.email}
-                      </span>
-                      <span className="text-sm text-gray-500 dark:text-gray-400">
-                        {user.gender == "female" ? "Feminino" : "Masculino"} - {user.dob.age} anos
-                      </span>
-                      <div className="mt-4 flex space-x-3 lg:mt-6">
-                        <Tooltip
-                          trigger="hover"
-                          content={`Endereço: ${user.location.street.name}  ${user.location.street.number}, ${user.location.city} - ${user.location.state} - ${user.location.country}`}
-                        >
-                          <div className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Localização
-                          </div>
-                        </Tooltip>
-                        <Tooltip
-                          trigger="hover"
-                          content={`Telefone: ${user.phone} - Celular: ${user.cell}`}
-                        >
-                          <div className="inline-flex items-center rounded-lg border border-gray-300 bg-white py-2 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
-                            Contato
-                          </div>
-                        </Tooltip>
-                      </div>
-                    </div>
-                  </Card>
-                </div>
-              </>
-            );
-          })}
-        </div>
-      </div>
+      <ListCard users={users} />
     </>
   );
 }
